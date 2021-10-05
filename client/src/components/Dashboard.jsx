741ed60667f553c015d4f8d6b29eb9182c8693a0
Swastikyadav <swastikyadav.dev@gmail.com>
2021-10-05 20:33:51 +0530

    added search input for news

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -20,6 +20,11 @@ function Dashboard() {
       <aside className="news-section">
         <h2 className="heading">Local Morning News</h2>
 
+        <form > 
+          <input className="search-input" type="search" placeholder="Search News Here..." />
+          <input className="search-btn" type="submit" value="Search" />
+        </form>
+
         <NewsCard />
         <NewsCard />
         <NewsCard />
