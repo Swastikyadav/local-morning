e836e842b538d735826ec77eec1c6c5f7420dae3
Swastikyadav <swastikyadav.dev@gmail.com>
2021-10-07 00:10:22 +0530

    added back to top link in postLists page

--- client/src/components/PostLists.jsx
+++ client/src/components/PostLists.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import PostCard from "./PostCard";
 
 import "../style/postLists.css";
@@ -5,6 +7,7 @@ import "../style/postLists.css";
 function PostLists() {
   return (
     <div className="post-lists">
+      <Link to="/">{`<<< Back To Home`}</Link>
       <h4>My Posts / Liked Posts</h4>
 
       <div className="post-cards">
