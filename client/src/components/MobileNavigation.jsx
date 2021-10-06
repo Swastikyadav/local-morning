3ff3ab4a30a6bfd63c8071e7944f2685b6576f08
Swastikyadav <swastikyadav.dev@gmail.com>
2021-10-06 19:22:09 +0530

    added links to mobile navigation and changed color when active

--- client/src/components/MobileNavigation.jsx
+++ client/src/components/MobileNavigation.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { HomeOutlined, RadiusUprightOutlined, UserOutlined, MessageOutlined } from "@ant-design/icons";
 
 import "../style/mobileNav.css";
@@ -6,9 +7,9 @@ function MobileNavigation() {
   return (
     <nav className="mobile-navigation">
       <div className="nav-links-container">
-        <HomeOutlined />
-        <RadiusUprightOutlined />
-        <UserOutlined />
+        <NavLink to="/dashboard/posts" activeClassName="active"><HomeOutlined /></NavLink>
+        <NavLink to="/dashboard/news" activeClassName="active"><RadiusUprightOutlined /></NavLink>
+        <NavLink to="/dashboard/profile" activeClassName="active"><UserOutlined /></NavLink>
         <MessageOutlined />
       </div>
     </nav>
