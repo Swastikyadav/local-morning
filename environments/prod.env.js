2a4db32b974273fd0ba38ac7e1c364a76c2539c9
Swastikyadav <swastikyadav.dev@gmail.com>
2021-10-18 13:18:28 +0530

    fixed typos

--- environments/prod.env.js
+++ environments/prod.env.js
@@ -1,12 +1,12 @@
 const prodEnvironment = {
-  dbUrl: "mongodb+srv://dbUserSwastik:dbUserSwastik@79n522my@cluster0.qa1eg.mongodb.net/localMorningDb?retryWrites=true&w=majority",
+  dbUrl: "mongodb+srv://dbUserSwastik:un9Ibu8jfNp7v7hW@cluster0.qa1eg.mongodb.net/localMorningDb?retryWrites=true&w=majority",
   jwtSecret: "prodSecret",
   sendGridApiKey: "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",
   googleClientId: "568866183233-385q4makm9hc823ic0qk4j5b1l40fbtp.apps.googleusercontent.com",
   googleCliendSecret: "ivESTYmnMur_RZPsAg6bwkK4",
   newsApiKey: "eaff2bf5273e498eabaf3fb454e290c0",
-  baseUrl: "https://localmorning.herokuapp.com/",
-  frontEndBaseUrl: "https://localmorning.herokuapp.com/",
+  baseUrl: "https://localmorning.herokuapp.com",
+  frontEndBaseUrl: "https://localmorning.herokuapp.com",
 }
 
 module.exports = prodEnvironment;
